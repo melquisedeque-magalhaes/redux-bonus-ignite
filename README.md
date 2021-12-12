387033e0a4937f391979d046a7c2dac81a421673
melquisedeque_sodre <melquisedeque.sodre.ext@dasa.com.br>
2021-12-11 23:26:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# redux-bonus-ignite
+ 
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Catalog } from './components/Catalog'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Redux</h1>
+      <Catalog />
+    </>
+  );
+}
+
+export default App;
